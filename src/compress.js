b37d2fedc0d2cc860e0ebcfa7cfb889b00b357e7
Yangfan <15234408101@163.com>
2018-06-13 15:05:19 +0800

    add: 字体base64转换

--- src/compress.js
+++ src/compress.js
@@ -5,20 +5,39 @@ module.paths.push(path.resolve(__dirname, '../node_modules'));
 
 const UglifyJS = require("uglify-js");
 const UglifyCSS = require("uglifycss");
+const { convertFont } = require("./convertbase64.js");
 
 const ROOT_PATH = path.resolve(__dirname, "../");
 
+let defaultCSSOption = {
+    "entry": [
+        "./examples/css/font-awesome.css"
+    ],
+    "output": {
+        "filename": "app.min.css",
+        "path": "./examples/compress/css"
+    },
+    "convertRelativePath": true,
+    "convertFontToBase64": false,
+    // "convertFontToBase64": {
+    //     "types": [".woff2", ".woff"],
+    //     "limit": 204800
+    // }
+};
+
 function Compress(option) {
+    console.log("----------初始化------------");
     this.opt = option;
     this.cssOpt = [];
     this.jsOpt = [];
     // init
     this.initFolder();
+    console.log("----------初始化完成------------");
 }
 Compress.prototype.initFolder = function () {
     // 区分css文件和js文件
     this.opt.forEach(function (conf, index) {
-        /.+\.js/g.test(conf.entry)
+        /.+\.js$/g.test(conf.entry)
             ? this.jsOpt.push(conf)
             : this.cssOpt.push(conf);
     }, this);
@@ -40,7 +59,9 @@ Compress.prototype.mkdirOfOutput = function (output) {
         : 0;
 };
 Compress.prototype.compressJS = function () {
+    let len=this.jsOpt.length;
     this.jsOpt.forEach(function (conf, index) {
+        console.log(`----------JS 压缩中 ${+1+index}/${len}------------`);
         // 创建输出路径
         this.mkdirOfOutput(conf.output.path);
         // 压缩混淆
@@ -48,13 +69,13 @@ Compress.prototype.compressJS = function () {
             return fs.readFileSync(url, "utf-8");
         }),
             {
-                sourceMap:conf.sourceMap?{
+                sourceMap: conf.sourceMap ? {
                     includeSources: true,
                     filename: `${conf.output.filename}.map`,
                     url: `${conf.output.filename}.min.map`,
                     // root: string;
                     // content: RawSourceMap;
-                }:false,
+                } : false,
                 output: {
                     comments: /license/i
                 }
@@ -65,27 +86,44 @@ Compress.prototype.compressJS = function () {
         // min code
         fs.writeFileSync(`${path.resolve(ROOT_PATH, conf.output.path)}/${conf.output.filename}`, res.code, "utf-8");
     }, this);
+    console.log("----------JS 压缩完成------------");
     return this;
 };
 Compress.prototype.minifyCSS = function () {
+    let len=this.cssOpt.length;
     this.cssOpt.forEach(function (conf, index) {
+        console.log(`----------CSS 压缩中 ${+1+index}/${len}------------`);
+        // conf=Object.assign(JSON.parse(JSON.stringify(defaultCSSOption)),conf);
         // 创建输出路径
         this.mkdirOfOutput(conf.output.path);
+        // 必须是相对路径，才能够转base64
+        if (typeof conf.convertFontToBase64==="object") {
+            conf.convertRelativePath = false;
+        }
         // 压缩
         var res = UglifyCSS.processFiles(conf.entry,
             {
                 // maxLineLen: 5,
-                convertUrls:conf.convertAbsolutePath?path.resolve(ROOT_PATH,"./examples"):"",
+                // convertUrls:conf.convertAbsolutePath?path.resolve(ROOT_PATH,"./examples"):"",
+                convertUrls: conf.convertRelativePath ? conf.entry[0] : false
                 // output:"js",
                 // expandVars: true
                 // uglyComments: true,
                 // cuteComments: true,
             }
         );
+        // ?转换字体为base64格式
+        conf.convertFontToBase64 && (res = convertFont({
+            txt: res,
+            cssPath: path.resolve(ROOT_PATH, conf.entry[0]), // BUG 
+            option: conf.convertFontToBase64
+        }));
+        // ?转换图片为base64格式 [TODO]
+
         //输出
         fs.writeFileSync(`${path.resolve(ROOT_PATH, conf.output.path)}/${conf.output.filename}`, res, "utf-8");
     }, this);
-
+    console.log("----------CSS 压缩完成------------");
     return this;
 };
 
---
+++ src/convertbase64.js
@@ -0,0 +1,43 @@
+/**
+ * BUG 1. 必须是压缩后的文件
+ *     2. 必须去除所有注释
+ */
+
+const fs = require('fs');
+const path = require('path');
+module.paths.push(path.resolve(__dirname, '../node_modules'));
+const mineType = require("mime-types");
+
+module.exports = {
+    convertFont: function ({txt,cssPath,option}) {
+        let url = {}; // cache font-url
+        // 转换路径
+        cssPath=path.parse(cssPath).dir;
+        // repalce
+        txt = txt.replace(/(?:url\()([^\)]+)\)/g, function ($1, $2, index, all) {
+            var initStr=`url(${$2})`;
+            url[`URL_${index}`] = path.resolve(cssPath,$2.replace(/[\'\"]+|(\?.*$)/g, ""));
+            // 判断文件类型
+            var ext=path.parse(url[`URL_${index}`]).ext;
+            if (!~(option.types.indexOf(ext))) {
+                console.warn(`${ext} 此文件类型不在转换范围内`);
+                return initStr;
+            }
+            // 读取字体文件
+            var fontPath = path.resolve(cssPath, url[`URL_${index}`]);
+            // 获取字体文件信息
+            var fontFileInfo = fs.statSync(fontPath);
+            if (fontFileInfo.size>option.limit) {
+                console.log(`文件超限，无法转换 path: ${fontPath}`);
+                return initStr;
+            }
+            var fontData = fs.readFileSync(fontPath);
+            // 转成base64
+            fontData = new Buffer(fontData).toString('base64');
+            // 返回替换
+            return `url('data:${mineType.lookup(fontPath)};base64,${fontData}')`;
+        });
+        // 写入css
+        return txt;
+    }
+}
