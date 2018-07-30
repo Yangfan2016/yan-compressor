660ef3ca43881a9659a51190e69d4e8d67beec7e
yangfan2016 <15234408101@163.com>
2018-07-30 17:06:37 +0800

    cli

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 const Compress = require("./compress.js");
-// const config = require("../config.json");
-
-// new Compress(config)
-//     .minifyCSS()
-//     .compressJS();
-
 
 module.exports=Compress;
